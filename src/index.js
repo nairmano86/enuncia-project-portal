2b1d15928b9e7cb718cd849abb9e68994a0bee05
nairmano86 <nairmano1407@gmail.com>
2025-04-10 01:40:55 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1>Welcome to Enuncia Project Portal 🚀</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
