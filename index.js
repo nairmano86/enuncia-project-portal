04b72d0b4678412a70183d898508684bbb2c96c7
nairmano86 <nairmano1407@gmail.com>
2025-04-10 00:40:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './index.css';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '10%' }}>
-      <img src="https://enuncia.global/assets/img/logo.svg" alt="Enuncia Global" height="80"/>
+      <img src="https://enuncia.global/assets/img/logo.svg" alt="Enuncia Global" height="80" />
       <h1>Welcome to the Enuncia Project Portal</h1>
       <p>Login system and dashboard coming soon.</p>
     </div>
