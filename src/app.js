b67a2b5b26a32adfd0335bda90c07e2004d030a4
nairmano86 <nairmano1407@gmail.com>
2025-04-11 00:47:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar'; // adjust path if needed
 import Home from './pages/Home';
-import Login from './pages/Login';
 import Register from './pages/Register';
-import EmployeeDashboard from './pages/EmployeeDashboard';
-import FreelancerDashboard from './pages/FreelancerDashboard';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
-        <Route path="/freelancer/dashboard" element={<FreelancerDashboard />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
