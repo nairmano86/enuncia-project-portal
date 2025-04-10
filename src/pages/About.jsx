36d655aab88f14d33d3d606afc959c10feb90175
nairmano86 <nairmano1407@gmail.com>
2025-04-11 01:16:24 +0530

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>About Page</h1>
+      <p>Welcome to the about section of Enuncia Project Portal.</p>
+    </div>
+  );
+};
+
+export default About;
