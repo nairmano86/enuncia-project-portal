3bb8ab3c89b8950de78b68558c2150efd2cb9dd0
nairmano86 <nairmano1407@gmail.com>
2025-04-11 01:01:36 +0530

    Update and rename app.js to app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './pages/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to Enuncia Project Portal 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
