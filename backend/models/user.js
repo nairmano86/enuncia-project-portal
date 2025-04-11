18947d3b13a9755629f169815234dc9baf644149
nairmano86 <nairmano1407@gmail.com>
2025-04-11 21:54:39 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['employee', 'freelancer'], default: 'freelancer' }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
 
