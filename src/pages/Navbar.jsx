423b58a778f02a91982f052fc635676d275d30df
nairmano86 <nairmano1407@gmail.com>
2025-04-11 11:10:23 +0530

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav style={{ padding: "1rem", backgroundColor: "#f5f5f5" }}>
-      <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+    <nav>
+      <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </nav>
   );
-};
+}
 
 export default Navbar;
