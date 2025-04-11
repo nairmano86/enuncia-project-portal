915a8afffafc532479a68d3a584ffbe8d06bcb42
nairmano86 <nairmano1407@gmail.com>
2025-04-11 11:30:03 +0530

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/">Home.jsx</Link>
+      <Link to="/about">About.jsx</Link>
     </nav>
   );
 }
