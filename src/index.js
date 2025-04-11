07eb5e177f62baebaa3de5057a7408aa70e398fc
nairmano86 <nairmano1407@gmail.com>
2025-04-11 18:27:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // double-check casing
+import App from './App.js'; // double-check casing
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
