845d2531b1c96e6bb2e196905963c32acce876f2
nairmano86 <nairmano1407@gmail.com>
2025-04-11 13:44:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-<Router>
-  <Navbar />
-  <div style={{ padding: '20px' }}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </div>
-</Router>
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
