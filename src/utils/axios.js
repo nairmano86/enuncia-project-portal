b6eef2f3986fd6ddb85e290bf5f4a99f163bf2a5
nairmano86 <nairmano1407@gmail.com>
2025-04-12 20:53:33 +0530

    Create axios.js

---
+++ src/utils/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// Create an axios instance with default settings
+const instance = axios.create({
+  baseURL: 'https://enuncia-backend.onrender.com/api', // Replace with your backend URL
+  headers: {
+    'Content-Type': 'application/json', // Default content type for requests
+  },
+});
+
+export default instance;
