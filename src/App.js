5f0881ddd0ac35725a36e989615acc9077f4362b
nairmano86 <nairmano1407@gmail.com>
2025-04-14 10:18:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import EmployeeDashboard from './pages/EmployeeDashboard';
+import FreelancerDashboard from './pages/FreelancerDashboard';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+        <Route path="/freelancer-dashboard" element={<FreelancerDashboard />} />
       </Routes>
     </Router>
   );
